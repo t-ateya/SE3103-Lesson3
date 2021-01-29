98a7f54b8872bd3810ee027d5b943c8332010a0b
Terence Ateya <ateyaterence@gmail.com>
2021-01-29 16:30:58 -0600

    first commit

---
+++ model/Question.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Question {
+	private String text; //Question
+	private String answer;
+
+	public Question(String text, String answer){
+		setText(text);
+		setAnswer(answer);
+	}
+
+	public String getText(){
+		return text;
+	}
+
+	public void setText(String text){
+		this.text = text;
+	}
+
+	public String getAnswer(){
+		return answer;
+	}
+
+	public void setAnswer(String answer){
+		this.answer = answer;
+	}
+
+	public boolean checkAnswer(String response){
+		return response.equalsIgnoreCase(answer);
+	}
+
+	
+	
+}
